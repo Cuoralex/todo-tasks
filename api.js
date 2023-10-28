8074ad622eae806cb858ff3abca63971f7ea42a7
Cuoralex <alexacuartas@gmail.com>
2023-10-28 08:23:07 -0500

    Server express, Mongo Conexion

--- api.js
+++ api.js
@@ -35,4 +35,40 @@ mongoose.connect(db, {
     }
 });
 
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+    var newTask = new TaskModel(task);
+
+    newTask.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+        Name: req.body.Name,
+        Deadline: req.body.Deadline
+    }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
 module.exports = router;
