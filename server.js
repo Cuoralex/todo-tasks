d10d8d738bcddad042330cd50c05ed3d618ea826
Judy Cuartas <alexacuartas@gmail.com>
2023-11-11 09:21:14 -0500

    CRUD ok

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const api = require('./api');
 const port = process.env.PORT || 3000;
 const app = express();
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.listen(port, function () {
